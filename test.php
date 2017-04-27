338dd71147f0332820510b0b8006179f22e504c7
tiananwei <awei.tian@qq.com>
2017-04-27 13:34:35 +0800

    ioc container for dev

---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+require "vendor/autoload.php";
+$c = new \tian\container();
---
+++ tests/ContainerTest.php
@@ -0,0 +1,65 @@
+<?php
+class ContainerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testMake()
+    {
+    	//$app = new \tian\container();
+    	//$c = $app->make('c');
+        //$this->assertTrue($c instanceof c, 'ok');
+        //$this->assertInstanceOf("c", $c);
+    }
+    public function testBind()
+    {
+    	$app = new \tian\container();
+    	$c = $app->bind('b', function($o) use ($app) {
+    		$this->assertEquals($o, $app);
+    		//这个地方不能用 return $app['b'];因为会递归解析这个B，没有退出条件
+    		return $app->build("b");
+    	});
+    	$this->assertInstanceOf("b", $c['b']);
+    }
+    public function testInstance()
+    {
+    	$app = new \tian\container();
+    	$c = $app->instance('gaga', 123);
+    	$this->assertEquals(123, $c->make('gaga'));
+    }
+}
+class a
+{
+	public function __construct(b $b)
+	{
+		$b->test();
+	}
+	public function test()
+	{
+		//echo 'a->test()'."\n";
+	}
+
+}
+class b
+{
+	private $c;
+	public function __construct($c=2)
+	{
+		$this->c = $c;
+	}
+	public function test()
+	{
+		//echo 'b->test():'.$this->c."\n";
+	}
+
+}
+class c
+{
+	public function __construct(a $a)
+	{
+		$a->test();
+	}
+}
+
